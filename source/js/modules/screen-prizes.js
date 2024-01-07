a50ef16a3f028f5a769b0218327d0076ed552a4a
Maxim Timofeev <hallo.bonjour.salut@gmail.com>
2024-01-07 14:47:37 +0300

    3.12. Реализовываем анимацию иллюстрации главного приза

--- source/js/modules/screen-prizes.js
+++ source/js/modules/screen-prizes.js
@@ -10,4 +10,24 @@ export default () => {
   );
 
   setupPageTitleAnimation(`.screen--prizes`, titleAnimation);
+
+  const mainPrizeAnimation = document.querySelector(`#baloon_entry_animation`);
+
+  document.body.addEventListener(`screenChanged`, (evt) => {
+    const {
+      detail: {screenName},
+    } = evt;
+
+    if (screenName === `prizes`) {
+      if (mainPrizeAnimation) {
+        setTimeout(() => {
+          mainPrizeAnimation.beginElement();
+        }, 250);
+      }
+    } else {
+      if (mainPrizeAnimation) {
+        mainPrizeAnimation.endElement();
+      }
+    }
+  });
 };
