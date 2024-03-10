1aa285c65039d1203237750bf49e4eae6667670d
Maxim Timofeev <hallo.bonjour.salut@gmail.com>
2024-03-10 23:21:28 +0300

    4.3. Реализовываем анимацию количества призов

--- source/js/modules/screen-prizes.js
+++ source/js/modules/screen-prizes.js
@@ -1,5 +1,6 @@
 import {LetterByLetterAnimation} from "./letter-by-letter-animation";
 import {setupPageTitleAnimation} from './setup-page-title-animation';
+import {Countup} from './CountUp';
 
 export default () => {
   const titleAnimation = new LetterByLetterAnimation(
@@ -13,21 +14,55 @@ export default () => {
 
   const mainPrizeAnimation = document.querySelector(`#baloon_entry_animation`);
 
+  // анимация цифр 2-го приза
+  const secondPrizeValueAnimation = new Countup(1, 7, 500, 12);
+
+  const secondPrizeValueEl = document.querySelector(`.prizes__item--cases b`);
+
+  secondPrizeValueAnimation.onUpdate = (val) => {
+    secondPrizeValueEl.textContent = val;
+  };
+
+  // анимация цифр 3-го приза
+  const thirdPrizeValueAnimation = new Countup(11, 900, 1000, 12);
+
+  const thirdPrizeValueEl = document.querySelector(`.prizes__item--codes b`);
+
+  thirdPrizeValueAnimation.onUpdate = (val) => {
+    thirdPrizeValueEl.textContent = val;
+  };
+
+  let played = false;
+
   document.body.addEventListener(`screenChanged`, (evt) => {
     const {
       detail: {screenName},
     } = evt;
 
+    if (!mainPrizeAnimation) {
+      return;
+    }
+
     if (screenName === `prizes`) {
-      if (mainPrizeAnimation) {
-        setTimeout(() => {
+      setTimeout(() => {
+        if (!played) {
           mainPrizeAnimation.beginElement();
-        }, 250);
-      }
+          played = true;
+        }
+      }, 250);
+
+      setTimeout(() => {
+        secondPrizeValueEl.textContent = 1;
+        secondPrizeValueAnimation.start();
+      }, 5000);
+
+      setTimeout(() => {
+        thirdPrizeValueEl.textContent = 11;
+        thirdPrizeValueAnimation.start();
+      }, 6500);
     } else {
-      if (mainPrizeAnimation) {
-        mainPrizeAnimation.endElement();
-      }
+      secondPrizeValueAnimation.stop();
+      thirdPrizeValueAnimation.stop();
     }
   });
 };
